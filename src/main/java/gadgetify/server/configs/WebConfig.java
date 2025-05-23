c2099aa76d8135399a07ec07aa07c0ae3c6f95fd
DESKTOP-4J4MD87\PC <hmoi27509@gmail.com>
2025-05-24 01:21:09 +0700

    First commit

---
+++ src/main/java/gadgetify/server/configs/WebConfig.java
@@ -0,0 +1,28 @@
+package gadgetify.server.configs;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.i18n.AcceptHeaderLocaleResolver;
+
+import java.util.Locale;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods("*")
+                .allowedHeaders("*")
+                .maxAge(3600);
+    }
+    @Bean
+    public LocaleResolver localeResolver() {
+        var resolver = new AcceptHeaderLocaleResolver();
+        resolver.setDefaultLocale(Locale.forLanguageTag("vi"));
+        return resolver;
+    }
+}
